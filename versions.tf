d493fb53e2bc79b8477256dd5e219ba169d60f67
Balaji Mariyappan <engbalaji@users.noreply.github.com>
2024-04-01 12:50:40 -0400

    Updated the versions.tf file for kubectl command use

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
-
 terraform {
-  required_version = ">= 1.0"
+  required_version = ">= 0.13"
+
   required_providers {
     kubectl = {
       source  = "gavinbunney/kubectl"
-      version = ">= 1.14.0"
+      version = ">= 1.7.0"
     }
   }
 }
