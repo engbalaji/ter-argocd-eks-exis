37cff3347216b1845dddea63e1b184c00e07deca
Balaji Mariyappan <engbalaji@users.noreply.github.com>
2024-04-06 11:36:42 -0400

    test

--- variables.tf
+++ variables.tf
@@ -1,16 +1,62 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
 variable "region" {
-  default = "us-east-1"
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
 }
 
-variable "eks-name" {
-  type    = string
-  default = "pepoc-eks-cluster"
+variable "release_name" {
+  type        = string
+  description = "Helm release name"
+  default     = "argocd"
+}
+variable "namespace" {
+  description = "Namespace to install ArgoCD chart into"
+  type        = string
+  default     = "argocd"
 }
 
-variable "env" {
-  default = "dev"
+variable "argocd_chart_version" {
+  description = "Version of ArgoCD chart to install"
+  type        = string
+  default     = "5.27.1" # See https://artifacthub.io/packages/helm/argo/argo-cd for latest version(s)
 }
 
-variable "kubeconfigfile" {
-  default = "/mnt/workspace/config"
+# Helm chart deployment can sometimes take longer than the default 5 minutes
+variable "timeout_seconds" {
+  type        = number
+  description = "Helm chart deployment can sometimes take longer than the default 5 minutes. Set a custom timeout here."
+  default     = 800 # 10 minutes
+}
+
+variable "admin_password" {
+  description = "Default Admin Password"
+  type        = string
+  default     = "Welcome#77"
+}
+
+variable "values_file" {
+  description = "The name of the ArgoCD helm chart values file to use"
+  type        = string
+  default     = "values.yaml"
+}
+
+variable "enable_dex" {
+  type        = bool
+  description = "Enabled the dex server?"
+  default     = true
+}
+
+variable "insecure" {
+  type        = bool
+  description = "Disable TLS on the ArogCD API Server?"
+  default     = false
+}
+
+variable "vpc_use" {
+  type        = string
+  description = "VPC to be used"
+  default     = "vpc-b30658d4"
 }
